ab81cee66e1257c72acc95eb705ee7eaa290802f
Nastaranjenab <Nastaran.jenabali@yahoo.com>
2022-11-03 22:30:43 +0100

    updated cv

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,7 @@ export const Profile = () => {
             <div className='profile-content'>
                 <h1 class="profile-header">Nastaran Jenabali</h1>
                 <h6>A full stack Web developer who is passionate about creating interactive applications and experiences on the web.</h6>
-                <div className='fill'><a target="_blank"  rel="noopener noreferrer" href="https://drive.google.com/file/d/1NjkdBhKJTbdSFioTwlsyt7skrhmBPVQT/view?usp=sharing">Checkout My Resume</a> </div>
+                <div className='fill'><a target="_blank"  rel="noopener noreferrer" href="https://drive.google.com/file/d/1QC7JbT8fQ5vcielIiKMStfKH6GgX_uXd/view?usp=sharing">Checkout My Resume</a> </div>
             </div>
             <img src={Nasi} alt='Profile'></img>
         </div>
